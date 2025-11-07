c3b0d2b294fc42ac78bcbeb186f8e114cf69ca73
macadam2903 <macadammaci@gmail.com>
2025-11-07 11:13:04 +0100

    changes

--- eva/backend_server.js
+++ eva/backend_server.js
@@ -3,6 +3,8 @@ const app = express();
 app.get('/', (req, res) => { 
   res.send('Hello from EVA Backend Server!');
 });
+app.get('/tavasz', (req, res) => {res.send('Hello Tavasz!');});
+
 app.listen(3000, () => {
   console.log('EVA Backend Server is running on http://localhost:3000');
 });
