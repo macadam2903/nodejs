ee8f91ea9d0be91dbf683164df5d12d9d949c448
macadam2903 <macadammaci@gmail.com>
2025-11-07 10:20:27 +0100

    first commit

---
+++ helloworld/index.js
@@ -0,0 +1,5 @@
+import * as math from './tools.js';
+
+console.log("Hello, World!");
+console.log(math.osszead(5, 3));
+console.log(math.kivon(5, 3));
---
+++ helloworld/tools.js
@@ -0,0 +1,8 @@
+export function osszead(a, b) {
+    return a + b;
+}
+export function kivon(a, b) {
+    return a - b;
+}
+//no default export lehet egyszer egy fájlban
+
