5e2c7724f8556a237f338c877197c93cf8be3a85
macadam2903 <macadammaci@gmail.com>
2025-11-13 14:39:17 +0100

    111

--- 11-13/body/index.js
+++ 11-13/body/index.js
@@ -7,3 +7,6 @@ app.post('/user', (req, res) => {
     let { name, age } = req.body;
     res.send(`User name is: ${name} and age is: ${age}`);
 }   );
+app.listen(3000, () => {
+    console.log('Server is running on port http://localhost:3000');
+});
