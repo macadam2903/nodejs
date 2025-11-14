a8432e1b0e52d8f413997d4b8db6a0b5c5839187
macadam2903 <macadammaci@gmail.com>
2025-11-14 09:33:20 +0100

    naptar

---
+++ 11-14/naptar/index.js
@@ -0,0 +1,31 @@
+import express from 'express'; 
+import fs from 'fs'; 
+import path from 'path'; 
+import { fileURLToPath } from 'url';
+ 
+const app = express();
+const port = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/:hetnapja', (req, res) => {
+    const hetnapja = req.params.hetnapja.toLowerCase();
+    const filePath = path.join(__dirname, 'public', `${hetnapja}.html`);
+console.log(`kérés érkezett a következp napra: ${hetnapja}`);
+
+    try {
+        if (fs.existsSync(filePath)) {
+            res.status(201).sendFile(filePath);
+        } else { 
+            res.status(404).send('<h1>A kért oldal nem található.');
+        }
+    } catch (err) {
+        res.status(500).send('<h1>Hiba történt a szerveren.');
+    }
+});
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
