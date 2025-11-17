b2b582c4c7157bd8383f4513ffed859ed4e2b999
macadam2903 <macadammaci@gmail.com>
2025-11-17 12:54:11 +0100

    port fix

--- gyumolcsos/app.js
+++ gyumolcsos/app.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 const dbConfig = {
   host: 'localhost',
   user: 'root',
-  password: '', // Módosítsd a saját jelszavadra
+  password: '', 
   database: 'fruits',
   port: 3307
 };
